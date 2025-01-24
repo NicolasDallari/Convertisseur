665bbf3d0678d43010608d5c8c0c644f35bbe567
dallari nicolas <nicogamer@hotmail.fr>
2025-01-24 11:14:29 +0100

    responsive phone fix

--- partials/header.php
+++ partials/header.php
@@ -9,9 +9,6 @@
     <meta name="author" content="VotreNomOuEntreprise">
     <meta name="robots" content="index, follow">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lavishly+Yours&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <title>Convertisseur de Devises </title>
 </head>
 
